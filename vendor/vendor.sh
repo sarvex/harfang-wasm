4c6c7edf8fe51ebe2b0174c6f5725d1eaadca4d0
Paul m. p. Peny <mail.peny@free.fr>
2022-11-28 07:01:05 +0100

    Update vendor.sh

--- vendor/vendor.sh
+++ vendor/vendor.sh
@@ -1,4 +1,7 @@
-export PACKAGES=${PACKAGES:-"emsdk harfang"}
+if $STATIC
+    export PACKAGES="emsdk harfang"
+else
+    export PACKAGES="emsdk"
+fi
 export VENDOR=harfang
 export LD_VENDOR="-sUSE_WEBGL2 -sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2 -sFULL_ES2 -sFULL_ES3" 
-# -sLEGACY_GL_EMULATION"
