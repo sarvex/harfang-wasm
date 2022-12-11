b91557d4359d777871887093ca4b2784bda864fd
pmp-p <mail.peny@free.fr>
2022-12-11 14:40:49 +0100

    test https://github.com/harfang3d/harfang3d/pull/38

--- vendor/vendor.sh
+++ vendor/vendor.sh
@@ -1,10 +1,9 @@
-if echo "$PACKAGES" |grep -q emsdk
+if echo $PACKAGES |grep -q harfang
 then
-    # mixed static
-    export PACKAGES="$PACKAGES harfang"
+    echo $PACKAGES
 else
-    # standalone
     export PACKAGES="emsdk harfang"
 fi
+
 export VENDOR=harfang
 export LD_VENDOR="$LD_VENDOR -sUSE_WEBGL2 -sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2 -sFULL_ES2 -sFULL_ES3" 
