696934d6542d8ff2137d1e98364a98680a2afc33
pmp-p <mail.peny@free.fr>
2022-12-12 13:19:54 +0100

    sync

--- vendor/vendor.sh
+++ vendor/vendor.sh
@@ -3,7 +3,7 @@ then
     echo $PACKAGES
 else
     export PACKAGES="emsdk harfang"
+    export STATIC=true
 fi
-
 export VENDOR=harfang
-export LD_VENDOR="$LD_VENDOR -sUSE_WEBGL2 -sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2 -sFULL_ES2 -sFULL_ES3" 
+export LD_VENDOR="$LD_VENDOR -sUSE_GLFW=3 -sUSE_WEBGL2 -sMIN_WEBGL_VERSION=2 -sMAX_WEBGL_VERSION=2 -sFULL_ES2 -sFULL_ES3"
